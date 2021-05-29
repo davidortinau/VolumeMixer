f9c239f254411c265b3020a3f2004e2e5832f045
davidortinau <david.ortinau@microsoft.com>
2021-05-29 09:29:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Volume Mixer
+
+This is an attempt to build a 4 channel mixer app for my old iPad Mini that can control my Windows machine.
+
+I'll be trying [MQTT](https://github.com/xamarin/mqtt) to communicate with Windows.
+
+The UI will initially be built with Xamarin.Forms and then ported to .NET MAUI.
---
+++ src/client/VolumeMixer/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VolumeMixer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("asdf", "tapped", "bye");
+        }
+
+        void DragGestureRecognizer_DragStarting(System.Object sender, Xamarin.Forms.DragStartingEventArgs e)
+        {
+            // allow it to move up/down along the track
+            BoxView thumb = (BoxView)sender;
+
+            
+        }
+    }
+}
